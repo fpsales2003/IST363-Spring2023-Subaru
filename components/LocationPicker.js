bb349c8837e8b743dfef38bb3dd9da1dc6587cb3
fpsales2003 <95240671+fpsales2003@users.noreply.github.com>
2023-04-04 18:21:05 -0400

    added sections to homepage

---
+++ components/LocationPicker.js
@@ -0,0 +1,11 @@
+import styles from './locationpicker.module.scss';
+import Container from './Container';
+
+const LocationPicker = () => {
+    return <section className={styles.locationPicker}>
+            <Container>
+            Location Picker stuff
+            </Container>
+        </section>
+}
+export default LocationPicker;
---
+++ components/SectionCTA.js
@@ -0,0 +1,8 @@
+import styles from './sectioncta.module.scss';
+
+const SectionCTA = () => {
+    return <section className={styles.sectionCta}>
+            SectionCTA
+        </section>
+}
+export default SectionCTA;
